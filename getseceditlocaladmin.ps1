4f135154f0cf57cb870704c87c45b9776959a7f5
Dyberg <23663375+Dyberg@users.noreply.github.com>
2022-04-25 11:53:10 +0200

    Add files via upload

---
+++ getseceditlocaladmin.ps1
@@ -0,0 +1,6 @@
+﻿$cred = Get-Credential
+$objects = Get-Content c:\users\supsebch\ServerList.txt
+ForEach ($object in $objects) {
+    Invoke-Command -ComputerName $object -Credential @cred -ScriptBlock { &'c:\path\secedit.exe' /export /cfg c:\OldSettings.txt }
+    Invoke-Command -ComputerName $object -Credential @cred -Scriptblock { &Get-LocalGroupMember -Group 'Administrators' | Export-CSV c:\OldLocalAdmins.txt }
+}
