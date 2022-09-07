f5ea85f5dd57d15081fb57f1ed1caec0c8df9922
Dyberg <23663375+Dyberg@users.noreply.github.com>
2022-09-07 16:00:22 +0200

    Update getseceditlocaladmin.ps1

--- getseceditlocaladmin.ps1
+++ getseceditlocaladmin.ps1
@@ -1,5 +1,5 @@
 ﻿$cred = Get-Credential
-$objects = Get-Content c:\users\supsebch\ServerList.txt
+$objects = Get-Content c:\users\USER\ServerList.txt
 ForEach ($object in $objects) {
     Invoke-Command -ComputerName $object -Credential @cred -ScriptBlock { &'c:\path\secedit.exe' /export /cfg c:\OldSettings.txt }
     Invoke-Command -ComputerName $object -Credential @cred -Scriptblock { &Get-LocalGroupMember -Group 'Administrators' | Export-CSV c:\OldLocalAdmins.txt }
