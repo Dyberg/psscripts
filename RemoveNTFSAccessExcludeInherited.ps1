746cdb2d4c9f69d32c5fd420ffe60616bb31ad5c
Dyberg <23663375+Dyberg@users.noreply.github.com>
2022-09-07 15:57:37 +0200

    Update RemoveNTFSAccessExcludeInherited.ps1

--- RemoveNTFSAccessExcludeInherited.ps1
+++ RemoveNTFSAccessExcludeInherited.ps1
@@ -1,1 +1,6 @@
-﻿ #Installation of NTFSSecurity might be needed:: Install-Module -Name NTFSSecurity -RequiredVersion 4.2.4  $share = "INSERT UNC PATH" $user = "DOMAIN\User or Group" Get-ChildItem -Path $share -Recurse |    Get-NTFSAccess -Account $user -ExcludeInherited |    Remove-NTFSAccess
+#Installation of NTFSSecurity might be needed:: Install-Module -Name NTFSSecurity -RequiredVersion 4.2.4 
+$share = "INSERT UNC PATH"
+$user = "DOMAIN\User or Group"
+Get-ChildItem -Path $share -Recurse |
+   Get-NTFSAccess -Account $user -ExcludeInherited |
+   Remove-NTFSAccess
