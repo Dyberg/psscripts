cd443d4119fd6a872b7a14cdb61d1bd6ef6a7f78
Dyberg <23663375+Dyberg@users.noreply.github.com>
2022-09-07 15:56:59 +0200

    Remove NTFS Access to folders for specific users.
    The script is recursive, which means it will do it for the target path, and all subfolders, and subfolders subfolders... (and so on)
    Can be edited to only do it on folders in specific path by removing "-recurse" form the script.
    
    Please use with caution, this script do not check before editing.

---
+++ RemoveNTFSAccessExcludeInherited.ps1
@@ -0,0 +1,1 @@
+﻿ #Installation of NTFSSecurity might be needed:: Install-Module -Name NTFSSecurity -RequiredVersion 4.2.4  $share = "INSERT UNC PATH" $user = "DOMAIN\User or Group" Get-ChildItem -Path $share -Recurse |    Get-NTFSAccess -Account $user -ExcludeInherited |    Remove-NTFSAccess
